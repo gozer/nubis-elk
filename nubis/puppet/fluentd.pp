e0370fc91455218a5c5b3a6cb5fdb3605cb96968
Justin Dow <justin@dowhaus.com>
2015-06-17 10:49:48 -0700

    adding fluentd elasticsearch plugin

--- nubis/puppet/fluentd.pp
+++ nubis/puppet/fluentd.pp
@@ -1,4 +1,11 @@
 include ::fluentd
+
+fluentd::install_plugin {
+    'es':
+        plugin_type => 'gem',
+        plugin_name => 'fluent-plugin-elasticsearch',
+}
+
 fluentd::configfile { 'collector': }
 fluentd::source { 'forwarder':
     configfile => "collector",
